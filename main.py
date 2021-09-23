798563e0841e671f3b917345a7f55df8035dd948
piotrmachut <piotr.machut@gmail.com>
2021-09-23 22:57:02 +0200

    Code improvements.

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+############### Python Blackjack House Rules #####################
+
+## The deck is unlimited in size. 
+## There are no jokers. 
+## The Jack/Queen/King all count as 10.
+## The the Ace can count as 11 or 1.
+## Use the following list as the deck of cards:
+## cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+## The cards in the list have equal probability of being drawn.
+## Cards are not removed from the deck as they are drawn.
+## The computer is the dealer.
+
 from ascii import logo
 from os import system, name
 import random
+
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 def clear():
     if name == 'nt':
@@ -8,10 +22,10 @@ def clear():
     else:
         _ = system('clear')
 
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 def pick_card(cards):
     return random.choice(cards)
+
 
 def calculate_score(score_table):
     total = 0
@@ -21,8 +35,27 @@ def calculate_score(score_table):
 
 
 def final_score(users_score, computers_score, users_cards, computers_cards):
-    print(f"wynik kompa: {computers_score} na kartach {computers_cards}") # TESTING PURPOSES
-    print(f"twój wynik: {users_score} na kartach {users_cards}") # TESTING PURPOSES
+    print(f"\n    Your final hand: {users_cards}, final score: {users_score}")
+    print(f"    Computer's final hand: {computers_cards}, final score {computers_score}")
+    winner = ""
+    users_draw = False
+    computers_draw = False
+    if users_score > 21:
+        users_draw = True
+    if computers_score > 21:
+        computers_draw = True
+    
+    if users_draw:
+        winner = "Computer win!"
+    elif not users_draw and users_score == computers_score:
+        winner = "Draw!"
+    elif not users_draw:
+        if users_score > computers_score:
+            winner = "You win!"
+        elif computers_score > users_score:
+            winner = "Computer win!"
+    
+    print(f"    {winner}\n")
 
 
 def game(cards):
@@ -32,17 +65,20 @@ def game(cards):
         users_cards.append(pick_card(cards))
         computers_cards.append(pick_card(cards))
     print(f"    Your cards: {users_cards}")
-    print(f"    Conputer's fitst card: {computers_cards[0]}")
+    print(f"    Conputer's first card: {computers_cards[0]} \n")
+    computers_score = calculate_score(computers_cards)
+    if computers_score < 17:
+        computers_cards.append(pick_card(cards))
+    computers_score = calculate_score(computers_cards)
     another_card = input("Type 'y' to get another card, type 'n' yo pass: ").lower()
     if another_card == "y":
         users_cards.append(pick_card(cards))
     users_score = calculate_score(users_cards)
-    computers_score = calculate_score(computers_cards)
-    if computers_score < 17:
-        computers_cards.append(pick_card(cards))
     final_score(users_score, computers_score, users_cards, computers_cards)
 
+
 app_running = True
+
 while app_running:
     game_start = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ").lower()
     clear()
